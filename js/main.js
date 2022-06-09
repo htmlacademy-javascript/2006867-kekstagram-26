63ef1be75b965647f28e5117341d036a8fd9ab55
Leisan Iusupova <leisan_11_@mail.ru>
2022-06-10 00:40:59 +0300

    Добавляет второе задание

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
-"use strict";
 
 // https://learn.javascript.ru/task/random-int-min-max
 function randomInteger(min, max) {
   // случайное число от min до (max+1)
   if (min >= max) {
-    console.log ('Сначала введите минимальное число отличное от максимального')
-    return
+    console.log ('Сначала введите минимальное число отличное от максимального');
+    return;
   }
-  let rand = min + Math.random() * (max + 1 - min);
+  const rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 }
 
